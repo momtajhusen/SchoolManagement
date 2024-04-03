2d5d4739f320237f2ec8928a5db474133560fa64
momtajhusen <mumtaz666raza@gmail.com>
2024-04-03 21:04:12 +0545

    update

--- public/admin_lang/developer/new-account-skip-school.js
+++ public/admin_lang/developer/new-account-skip-school.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var website = ['gurukul', 'sunrise'];
     var subdomain = window.location.hostname.split('.')[0];
  
-    if(subdomain != '127' || subdomain != 'dev'){
+    if(subdomain != '127' && subdomain != 'dev'){
         if (website.includes(subdomain)) {
             $('#new-account-menu').remove();
         }else{
