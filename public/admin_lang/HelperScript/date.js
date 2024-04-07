b65b2f23ac334e3cf832dcbf23718f7ac6bd2dab
momtajhusen <mumtaz666raza@gmail.com>
2024-04-07 22:14:20 +0545

    update

--- public/admin_lang/HelperScript/date.js
+++ public/admin_lang/HelperScript/date.js
@@ -3,18 +3,29 @@
 var subdomain = window.location.hostname.split('.')[0];
 let current_year, current_month, current_day, MonthsArray;
 
-if (subdomain == 'dev') {
-    current_year = 2080;
-    current_month = 1;
-    current_day = 1;
+let custom_on = 'OFF';
+
+if(custom_on == 'ON'){
+    if (subdomain == 'dev') {
+        current_year = 2080;
+        current_month = 1;
+        current_day = 1;
+    }else{
+        current_year = NepaliFunctions.GetCurrentBsDate().year;
+        current_month = NepaliFunctions.GetCurrentBsDate().month;
+        current_day = NepaliFunctions.GetCurrentBsDate().day;
+        decremented_current_month = current_month - 1;
+        MonthsArray = NepaliFunctions.GetBsMonths();
+    }   
+     
 }else{
     current_year = NepaliFunctions.GetCurrentBsDate().year;
     current_month = NepaliFunctions.GetCurrentBsDate().month;
     current_day = NepaliFunctions.GetCurrentBsDate().day;
     decremented_current_month = current_month - 1;
-    MonthsArray = NepaliFunctions.GetBsMonths();
-}   
- 
+    MonthsArray = NepaliFunctions.GetBsMonths(); 
+}
+
 
 
 
