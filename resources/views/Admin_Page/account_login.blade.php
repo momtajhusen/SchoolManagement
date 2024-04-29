7702335b17049701814ae538caeb244f1d404615
momtajhusen <mumtaz666raza@gmail.com>
2024-04-29 21:28:08 +0545

    update

--- resources/views/Admin_Page/account_login.blade.php
+++ resources/views/Admin_Page/account_login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ACCOUNT LOGIN</title>
+    <title>ACCOUNT</title>
         <!-- Main CSS -->
         <link rel="stylesheet" href="admin_template_assets/css/main.css">
         <!-- Bootstrap CSS -->
